9f33c0c5bedf6050a9ca10ab793f38cd2471c8af
guy-he <75090060+guy-he@users.noreply.github.com>
2020-11-26 19:33:11 +0200

    Create program.java

---
+++ program.java
@@ -0,0 +1,33 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        int min_value = -1;
+                int max_value = +100;
+//comment
+  byte player_level = 1;
+  float date = 23.11f;
+  double targil = 8.8876;
+  boolean human = true;
+        System.out.println(player_level);
+        System.out.println(date);
+        System.out.println(targil);
+        System.out.println(human);
+int q = 181335;
+        float a = 33.54f;
+                char lettex = 'x';
+           char letty = 'y';
+           short xs = 1, ys = 2, zs = 3;
+           float t = 4.3f * 7.8f;
+        System.out.println(q);
+        System.out.println(a);
+        System.out.println(lettex);
+        System.out.println(letty);
+        System.out.println(xs);
+        System.out.println(t);
+
+    }
